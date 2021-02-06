df77bd8ffffae0a91d93d3f1734c3d77c3a9196c
Ryan Barnes <rpbarnes9@gmail.com>
2021-02-06 07:25:59 -0700

    initial commit

---
+++ ProtectedShoutUser.cs
@@ -0,0 +1,21 @@
+﻿
+namespace IdentityModel
+{
+
+    // Add profile data for application users by adding properties to the ShoutUser class
+    public class ProtectedShoutUser
+    {
+        public string UserName { get; set; }
+        public string Id { get; set; }
+        public string ProfileUrl { get; set; } = "https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=675.0&fit=crop";
+
+        public ProtectedShoutUser(ShoutUser user) 
+        {
+            UserName = user.UserName;
+            Id = user.Id;
+        }
+
+        public ProtectedShoutUser() 
+        { }
+    }
+}
---
+++ ShoutUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityModel
+{
+    // Add profile data for application users by adding properties to the ShoutUser class
+    public class ShoutUser : IdentityUser
+    {
+    }
+}
